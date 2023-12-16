a020e33c5ea061928981fb236fde918278764c98
santijaga <silverstarlingstudio@gmail.com>
2023-12-16 16:38:30 +0100

    Android Setup

--- Source/MergeHouses/MergeHouses.Build.cs
+++ Source/MergeHouses/MergeHouses.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class MergeHouses : ModuleRules
 {
@@ -11,6 +12,12 @@ public class MergeHouses : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		if (Target.Platform == UnrealTargetPlatform.Android)
+		{
+			var manifestFile = Path.Combine(ModuleDirectory, "AndroidSanitizePermissions_UPL.xml");
+			AdditionalPropertiesForReceipt.Add("AndroidPlugin", manifestFile);
+		}
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
